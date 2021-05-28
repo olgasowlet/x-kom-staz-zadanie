14bf4abec1a23b7c1891066cc0fa0449c4f508a7
Olga Sowinska <olga.sowinska00@gmail.com>
2021-05-28 12:51:20 +0200

    created view of the screening room

--- src/features/ScreeningRoom/index.js
+++ src/features/ScreeningRoom/index.js
@@ -6,9 +6,11 @@ const ScreeningRoom = () => {
     const { seats } = useSelector(selectSeats);
     console.log(seats);
 
+    //Mogę zrobić tak, że dodam atrybut x do każdego Seat i w styled komponent jeśli taki atrybut będzie miał wartość np. 0 to będzie w pierwszym wierszu itd.
+
     return (
         <Screen>
-            {seats.map(seat => <Seat key={seat.id}>{seat.id}</Seat>)}
+            {seats.map(seat => <Seat key={seat.id} column={seat.cords.y} row={seat.cords.x}>{seat.id}</Seat>)}
         </Screen>
     );
 };
--- src/features/ScreeningRoom/styled.js
+++ src/features/ScreeningRoom/styled.js
@@ -1,13 +1,21 @@
 import styled from "styled-components";
 
+const handlePosition = (position) => {
+    return position + 1;
+}
+
 export const Screen = styled.div`
     display: grid;
     width: 100%;
-    grid-template-columns: repeat(auto-fill, 100px);
+    grid-template-columns: repeat(auto-fill, 70px);
+    row-gap: 10px;
+    column-gap: 10px;
 `;
 
 export const Seat = styled.div`
-    width: 100px;
-    height: 100px;
+    width: 70px;
+    height: 70px;
     border: 1px solid black;
+    grid-row: ${({ row }) => handlePosition(row)};
+    grid-column: ${({ column }) => handlePosition(column)};
 `;
