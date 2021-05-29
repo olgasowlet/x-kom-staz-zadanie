ef5b9a7292a1250856704c7290e9c174414185ef
Olga Sowinska <olga.sowinska00@gmail.com>
2021-05-29 17:25:18 +0200

    added limit which controls quantity of checked seats

--- src/features/ScreeningRoom/index.js
+++ src/features/ScreeningRoom/index.js
@@ -1,38 +1,45 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { addSeat, removeSeat, selectSeats } from "../../app/seatsSlice";
+import { addSeat, removeSeat, selectQtySBU, selectSeats, selectSeatsSBU } from "../../app/seatsSlice";
 import { Screen, Seat } from "./styled";
 import Main from "../../common/Main";
 
 const ScreeningRoom = () => {
     const { seats } = useSelector(selectSeats);
+    const qtySBU = useSelector(selectQtySBU);
+    const seatsSBU = useSelector(selectSeatsSBU)
     const dispatch = useDispatch();
 
     const onSeatChange = (event, id, x, y) => {
         if (!event.target.checked) {
             dispatch(removeSeat(id));
         } else {
-            dispatch(addSeat({
-                id,
-                cords: {
-                    x,
-                    y
-                },
-            }))
+            if (seatsSBU.length < qtySBU) {
+                dispatch(addSeat({
+                    id,
+                    cords: {
+                        x,
+                        y
+                    },
+                }))
+            } else {
+                event.target.checked = false;
+                alert(`Nie możesz wybrać więcej niż ${qtySBU} miejsc`)
+            }
         };
     }
 
     return (
         <Main screen>
             <Screen>
-                {seats ? seats.map(seat => 
-                <Seat 
-                    key={seat.id} 
-                    column={seat.cords.y} 
-                    row={seat.cords.x} 
-                    disabled={seat.reserved} 
-                    type="checkbox" 
-                    onInput={(event) => onSeatChange(event, seat.id, seat.cords.x, seat.cords.y)} 
-                />) : "Trwa ładowanie danych"}
+                {seats ? seats.map(seat =>
+                    <Seat
+                        key={seat.id}
+                        column={seat.cords.y}
+                        row={seat.cords.x}
+                        disabled={seat.reserved}
+                        type="checkbox"
+                        onInput={(event) => onSeatChange(event, seat.id, seat.cords.x, seat.cords.y)}
+                    />) : "Trwa ładowanie danych"}
             </Screen>
         </Main>
     );
