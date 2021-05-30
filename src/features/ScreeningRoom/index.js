53ca081bd7d2a1247f710c962cbf172225d5ed01
Olga Sowinska <olga.sowinska00@gmail.com>
2021-05-30 18:14:01 +0200

    created functions onchech

--- src/features/ScreeningRoom/index.js
+++ src/features/ScreeningRoom/index.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { addSeat, getSideBySideSeats, removeAllSeats, removeSeat, selectQtySBU, selectSeats, selectSeatsSBU, selectSideBySide } from "../../app/seatsSlice";
+import { addSeat, setSideBySideSeats, removeAllSeats, removeSeat, selectQtySBU, selectSeats, selectSeatsSBU, selectSideBySide } from "../../app/seatsSlice";
 import { Screen, Seat } from "./styled";
 import Main from "../../common/Main";
 
@@ -10,44 +10,43 @@ const ScreeningRoom = () => {
     const sideBySide = useSelector(selectSideBySide);
     const dispatch = useDispatch();
 
-    const reactOnReservedSeats = () => {
-        alert("Nie da się wybrać miejsc obok siebie, proszę wybrać inne miejsca");
-        dispatch(removeAllSeats());
-    }
-
-    const onSeatChange = (event, id, x, y) => {
-        if (!event.target.checked) {
-            dispatch(removeSeat(id));
+    const onChange = (event, id, x, y) => {
+        if (event.target.checked) {
+            onChecked(event, id, x, y);
         } else {
-            if (seatsSBU.length < qtySBU) {
-                if (seatsSBU.every(seat => seat.reserved === false)) {
-                    reactOnReservedSeats();
-                    event.target.checked = false;
-                } else if (sideBySide) {
-                    console.log(x, y);
-                    dispatch(getSideBySideSeats({
-                        id,
-                        cords: {
-                            x,
-                            y
-                        },
-                        qtySBU
-                    }));
-                } else {
-                    dispatch(addSeat({
-                        id,
-                        cords: {
-                            x,
-                            y
-                        },
-                    }))
-                }
+            onUnchecked(id)
+        }
+    };
+
+    const onChecked = (event, id, x, y) => {
+        if (seatsSBU.length < qtySBU) {
+            if (sideBySide) {
+                dispatch(setSideBySideSeats({
+                    id,
+                    cords: {
+                        x,
+                        y
+                    },
+                    qtySBU
+                }));
             } else {
-                event.target.checked = false;
-                alert(`Nie możesz wybrać więcej niż ${qtySBU} miejsc`)
+                dispatch(addSeat({
+                    id,
+                    cords: {
+                        x,
+                        y
+                    },
+                }))
             }
+        } else {
+            event.target.checked = false;
+            alert(`Nie możesz wybrać więcej niż ${qtySBU} miejsc`)
         };
     };
+
+    const onUnchecked = (id) => {
+        sideBySide ? dispatch(removeAllSeats()) : dispatch(removeSeat(id));
+    }
 
     return (
         <Main screen>
@@ -59,7 +58,7 @@ const ScreeningRoom = () => {
                         row={seat.cords.x}
                         disabled={seat.reserved}
                         type="checkbox"
-                        onInput={(event) => onSeatChange(event, seat.id, seat.cords.x, seat.cords.y)}
+                        onInput={(event) => onChange(event, seat.id, seat.cords.x, seat.cords.y)}
                     />) : "Trwa ładowanie danych"}
             </Screen>
         </Main>
