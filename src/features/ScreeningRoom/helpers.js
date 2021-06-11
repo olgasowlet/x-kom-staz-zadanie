6a6ba5b905ec7ee5d8a93f34e893c1d10aa769e0
Olga Sowinska <olga.sowinska00@gmail.com>
2021-06-11 11:05:05 +0200

    fixed function to filter arrays with length of quantity

--- src/features/ScreeningRoom/helpers.js
+++ src/features/ScreeningRoom/helpers.js
@@ -1,10 +1,10 @@
 const checkCordsY = (s, i, element) => {
     let result = 0;
 
-    if (typeof element[i+1] === `undefined`) {
-        result = s.cords.y - element[i-1].cords.y 
+    if (typeof element[i + 1] === `undefined`) {
+        result = s.cords.y - element[i - 1].cords.y
     } else {
-        result = element[i+1].cords.y - s.cords.y
+        result = element[i + 1].cords.y - s.cords.y
     }
 
     return result;
@@ -15,21 +15,21 @@ export const checkReservation = (seats, id, qtySBU, x) => {
         ...seats.filter(s => s.reserved === false && s.cords.x === x)
     ];
 
-    const index = seats.findIndex(s => s.id === id);
     const seatsy = [];
 
-    for (let i=0; seatsy.length < 15; i ++) {
+    for (let i = 0; seatsy.length < 15; i++) {
         seatsy.push(i);
     }
 
-    let propose = []
-    const tests = seats.map((s, index) => seats.slice(index, index + qtySBU))
-    const test = tests.map(s => s)
-    
-    propose = test.find(element => (element.some(s => s.id === id) && element.every((s, i, element) => checkCordsY(s, i, element) === 1)) === true);
+    seats = [
+        ...seats.map((s, index) => seats.slice(index, index + qtySBU)).filter(e => e.length === qtySBU),
+    ]
 
-    return (typeof propose === `undefined` ? [] : propose);
+    seats = seats.find(element => (element.some(s => s.id === id) && element.every((s, i, element) => checkCordsY(s, i, element) === 1)) === true)
+
+    return (typeof seats === `undefined` ? [] : seats);
 }
+
 
 export const getSeatsUnreserved = (seats, q) => {
     let unreservedSeats = [];
